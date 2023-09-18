b23ef6d9dc133aea9711efb9a4bcfaf166b367f7
Strageehh <138829214+Strageehh@users.noreply.github.com>
2023-09-18 19:31:14 +0000

    iiii

--- UI/UI/Main.php
+++ UI/UI/Main.php
@@ -3,11 +3,12 @@
 namespace UI;
 
 use pocketmine\plugin\PluginBase;
+use pocketmine\player\Player;
 use FormAPI\SimpleForm;
 
 class Main extends PluginBase
 {
   if ($player instanceof Player) {
-    $form->setTitle("Example U")
+    $form->setTitle("Example UI");
   }
 }
